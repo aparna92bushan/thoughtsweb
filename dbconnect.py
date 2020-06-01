5abaf8e22a9b4541ad747a1f11ced3f616f54632
aparnabushan <aparna.bushan@auto-grid.com>
2020-06-02 00:16:24 +0530

    test push

--- dbconnect.py
+++ dbconnect.py
@@ -1,14 +1,17 @@
 from MySQLdb import _mysql
 
+
 def connection():
     db = _mysql.connect(host="localhost", user="root", passwd="password", db="thoughtsweb_db")
     return db
+
 
 def fetch_thoughts():
     db = connection()
     db.query("select body from thoughts where display_date = CURDATE()")
     results = db.store_result()
     return results.fetch_row()
+
 
 def submit_thoughts_db(thought):
     print(thought)
